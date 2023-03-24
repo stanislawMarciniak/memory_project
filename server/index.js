b3fcef52a025135ce05a6a4b8f29c5b18c9854c7
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-03-25 00:34:19 +0100

    setup mongo

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  "mongodb+srv://sstasio:marciniak13e@cluster0.xoszs5b.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://marciniak13ee:marciniak13e@cluster0.xoszs5b.mongodb.net/?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
