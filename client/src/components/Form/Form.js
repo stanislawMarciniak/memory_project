818e62c5874e5ece50804899b1b5112c15827090
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-04-09 23:44:40 +0200

    form and posts

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 import useStyles from "./styles.js";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
   const classes = useStyles();
 
-  return <h1>FORM</h1>;
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
